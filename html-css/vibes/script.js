25d7c45053856560bc613cf5fe697898ad680a98
laurecohen <laure.chn@gmail.com>
2020-07-05 00:26:00 +0200

    Cleanup css files + jquery menu, lists and dark-mode

--- html-css/vibes/script.js
+++ html-css/vibes/script.js
@@ -1,13 +1,51 @@
 $( document ).ready(function() {
+    // Set Initial Color Palette
+    $('.template-section:nth-child(odd)').addClass('s-odd');
+    $('.template-section:nth-child(even)').addClass('s-even');
+    
+    var $menu = '.menu-xs';
+    $('.main-menu--toggle').click(function(){
+        // Display/hide the menu and switch trigger-icon
+        $($menu).toggleClass('expanded');
+        $(this).children().toggle();
+    });
+
+    $( '.list-icon' ).click(function(){
+        // Display/hide accordion and toggle fontawesome class
+        $(this).toggleClass('accordion-open').children().hasClass('fa-plus') ? 
+            $(this).children().removeClass('fa-plus').addClass('fa-minus') :
+            $(this).children().removeClass('fa-minus').addClass('fa-plus');
+    });
+
+    $('.color-mode--toggle').click(function(){
+        // Switch colors…
+        $('body').toggleClass('b-dark');
+        $('.logo').toggleClass('l-dark');
+        $('.s-even').toggleClass('s-dark');
+    });
+    
+    // Function resize 
     $(window).resize(function(){
         var $deviceWidth = $(window).width();
+        
+        // Check if browser screen width has changed
         if( $deviceWidth >= 576){
-            $('.menu-xs').css('display', 'none');
+
+            // Check if menu has remained open during the screen orientation/width change
+            if( $($menu).hasClass('expanded')) {
+                // … then hide it again and switch the trigger-icon again
+                $($menu).removeClass('expanded');
+                $('.main-menu--toggle').children().toggle();
+            }
         }
     });
 
-    $('.main-menu--toggle').click(function(){
-        $(this).children().toggle();
-        $('.menu-xs').toggle();
+    $(window).scroll(function(){
+        
+        if( $(window).scrollTop() ){
+            $('.site-header').addClass('p-sticky');        
+        } else {
+            $('.site-header').removeClass('p-sticky');
+        }
     });
 });
