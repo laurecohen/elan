32b6ebfefc08d518dccbba2c2d530a8b083b81e8
laurecohen <laure.chn@gmail.com>
2020-07-06 20:53:11 +0200

    color-mode html

--- html-css/vibes/script.js
+++ html-css/vibes/script.js
@@ -1,27 +1,28 @@
 $( document ).ready(function() {
-    // Set Initial Color Palette
+    // Set Template Initial Color Palette
     $('.template-section:nth-child(odd)').addClass('s-odd');
     $('.template-section:nth-child(even)').addClass('s-even');
     
-    var $menu = '.menu-xs';
-    $('.main-menu--toggle').click(function(){
+    var menu = $('.menu-xs');
+    var trigger = $('.main-menu--toggle');
+
+    trigger.click(function(){
         // Display/hide the menu and switch trigger-icon
-        $($menu).toggleClass('expanded');
+        menu.toggleClass('expanded');
         $(this).children().toggle();
-    });
+    });  
 
     $( '.list-icon' ).click(function(){
         // Display/hide accordion and toggle fontawesome class
-        $(this).toggleClass('accordion-open').children().hasClass('fa-plus') ? 
-            $(this).children().removeClass('fa-plus').addClass('fa-minus') :
-            $(this).children().removeClass('fa-minus').addClass('fa-plus');
+        ( $(this).toggleClass('accordion-open').children().hasClass('fa-plus') ) ? 
+            ( $(this).children().removeClass('fa-plus').addClass('fa-minus') ) :
+            ( $(this).children().removeClass('fa-minus').addClass('fa-plus') );
     });
 
+    // Color-mode
     $('.color-mode--toggle').click(function(){
-        // Switch colors…
-        $('body').toggleClass('b-dark');
-        $('.logo').toggleClass('l-dark');
-        $('.s-even').toggleClass('s-dark');
+        // Set color-mode value to switch modes
+        ( $('html').attr('color-mode') === 'light' ) ? ( $('html').attr('color-mode', 'dark') ) : ( $('html').attr('color-mode', 'light') );
     });
     
     // Function resize 
@@ -30,22 +31,19 @@ $( document ).ready(function() {
         
         // Check if browser screen width has changed
         if( $deviceWidth >= 576){
-
+    
             // Check if menu has remained open during the screen orientation/width change
-            if( $($menu).hasClass('expanded')) {
+            if( $('menu-xs').hasClass('expanded')){
                 // … then hide it again and switch the trigger-icon again
-                $($menu).removeClass('expanded');
+                $('menu-xs').removeClass('expanded');
                 $('.main-menu--toggle').children().toggle();
             }
         }
     });
 
-    $(window).scroll(function(){
-        
-        if( $(window).scrollTop() ){
-            $('.site-header').addClass('p-sticky');        
-        } else {
-            $('.site-header').removeClass('p-sticky');
-        }
+    // Gallery
+    $('.gallery-grid-link').click(function(){
+        $(this).parent().children('.gallery-large').toggleClass('fullwidth');
+        // Add close btn
     });
 });
