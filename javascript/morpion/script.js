2eca336e84618d747b4d5edd6a8faaf413d0fc5d
laurecohen <laure.chn@gmail.com>
2020-07-13 16:27:24 +0200

    clean js folder

--- javascript/morpion/script.js
+++ javascript/morpion/script.js
@@ -1,44 +1,65 @@
-// Partie 0 manche 0 => player 1 = "x" (par défault)
+
 let player = "x"
-var nbCell = 9
-var nbClick = 0
-var finDuJeu = false
+let nbCell = 9
+let finDuJeu = false
 
 $(".cell").click(function(){
-    if( $(this).text() ===  "" && !finDuJeu ){
-        $(this).text(player)
+    if( $(this).text() ===  "" ){
         player = player === "o" ? "x" : "o"
+        $(this).text(player)
         nbCell--
-    }
-    
-    $(".cell").each(function(){
-        $rows = $(this).closest("tr").find(".cell").text()    
-        console.log($rows)
 
-        if ($rows === "xxx"){
-            msg = "x a gagné !"
-            finDuJeu = true
-        } else if($rows === "ooo"){
-            msg = "o a gagné !"
+        if (nbCell === 0){
             finDuJeu = true
         }
-    })  
-    
-    console.log(msg)
-    if (finDuJeu){
-        $(this).off("click")
     }
 
-    // Si toutes les cases (nbCell) sont remplies --> afficher "jeu terminé"
-    if (nbCell === 0){
-        console.log("Le jeu est terminé")
-        // Afficher dans un span .msg
+    if(finDuJeu){
+        console.log('Fin du jeu')
+    }
+
+    for(let x = 1; x <= 3; x++){
+        let ligne = ""
+        let colonne = ""
+        let diag1 = ""
+        let diag2 = ""
+        
+        for(let y = 1; y <= 3; y++){
+            ligne += $("#"+x+"-"+y).text()
+            colonne += $("#"+y+"-"+x).text()
+            
+            if(ligne === player.repeat(3)){
+                finDuJeu = true
+                return true
+            }
+            if(colonne === player.repeat(3)){
+                finDuJeu = true
+                return true
+            }
+            if (x == y){
+                diag1 += $("#"+y+"-"+x).text()
+                if(diag1 === player.repeat(3)){
+                    finDuJeu = true
+                    return true
+                }    
+            }
+            if (x + y == 4){
+                diag2 += $("#"+y+"-"+x).text()
+                if(diag2 === player.repeat(3)){
+                    finDuJeu = true
+                    return true
+                } else {
+                    return false
+                }
+            }
+        }
     }
 })
 
-$('#reset-table').click(function(){
+$("#reset-table").click(function(){
     // Vider toutes les cases et réinitialiser la variable nbCell
-    $('.cell').text('')
+    $(".cell").text("")
+    finDuJeu = false
     nbCell = 9
     console.clear()
 })
